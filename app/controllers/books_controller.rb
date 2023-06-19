30b39067109a0ba6ebe916339de6c284f2b2cda1
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-06-19 08:02:20 +0000

    [Add]投稿・保存機能

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+
+  #データの新規作成フォームを表示する
+  def new
+    @book = Book.new
+  end
+
+  #データを追加（保存）する
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  #データの一覧を表示する
+  def index
+
+  end
+
+  #データの内容（詳細）を表示する
+  def show
+
+  end
+
+  #データを更新するためのフォームを表示する
+  def edit
+
+  end
+
+  #データを削除する
+  def destroy
+
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,30 +5,37 @@ Rails.application.routes.draw do
   get 'homes/top'
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: "about"
-
-
+  resources :books
 
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-#                   Prefix Verb   URI Pattern                                                                                       Controller#Action
-#               homes_top GET    /homes/top(.:format)                                                                              homes#top
-#             homes_about GET    /homes/about(.:format)                                                                            homes#about
-#         new_user_session GET    /users/sign_in(.:format)                                                                          devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)                                                                          devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)                                                                         devise/sessions#destroy
-#       new_user_password GET    /users/password/new(.:format)                                                                     devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format)                                                                    devise/passwords#edit
-#           user_password PATCH  /users/password(.:format)                                                                         devise/passwords#update
-#                         PUT    /users/password(.:format)                                                                         devise/passwords#update
-#                         POST   /users/password(.:format)                                                                         devise/passwords#create
-# cancel_user_registration GET    /users/cancel(.:format)                                                                           devise/registrations#cancel
-#   new_user_registration GET    /users/sign_up(.:format)                                                                          devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)                                                                             devise/registrations#edit
-#       user_registration PATCH  /users(.:format)                                                                                  devise/registrations#update
-#                         PUT    /users(.:format)                                                                                  devise/registrations#update
-#                         DELETE /users(.:format)                                                                                  devise/registrations#destroy
-#                         POST   /users(.:format)                                                                                  devise/registrations#create
-#                     root GET    /                                                                                                 homes#top
+                #                   Prefix Verb   URI Pattern                                                                                       Controller#Action
+                #         new_user_session GET    /users/sign_in(.:format)                                                                          devise/sessions#new
+                #             user_session POST   /users/sign_in(.:format)                                                                          devise/sessions#create
+                #     destroy_user_session DELETE /users/sign_out(.:format)                                                                         devise/sessions#destroy
+                #       new_user_password GET    /users/password/new(.:format)                                                                     devise/passwords#new
+                #       edit_user_password GET    /users/password/edit(.:format)                                                                    devise/passwords#edit
+                #           user_password PATCH  /users/password(.:format)                                                                         devise/passwords#update
+                #                         PUT    /users/password(.:format)                                                                         devise/passwords#update
+                #                         POST   /users/password(.:format)                                                                         devise/passwords#create
+                # cancel_user_registration GET    /users/cancel(.:format)                                                                           devise/registrations#cancel
+                #   new_user_registration GET    /users/sign_up(.:format)                                                                          devise/registrations#new
+                #   edit_user_registration GET    /users/edit(.:format)                                                                             devise/registrations#edit
+                #       user_registration PATCH  /users(.:format)                                                                                  devise/registrations#update
+                #                         PUT    /users(.:format)                                                                                  devise/registrations#update
+                #                         DELETE /users(.:format)                                                                                  devise/registrations#destroy
+                #                         POST   /users(.:format)                                                                                  devise/registrations#create
+                #               homes_top GET    /homes/top(.:format)                                                                              homes#top
+                #                     root GET    /                                                                                                 homes#top
+                #                   about GET    /homes/about(.:format)                                                                            homes#about
+                #                   books GET    /books(.:format)                                                                                  books#index
+                #                         POST   /books(.:format)                                                                                  books#create
+                #                 new_book GET    /books/new(.:format)                                                                              books#new
+                #               edit_book GET    /books/:id/edit(.:format)                                                                         books#edit
+                #                     book GET    /books/:id(.:format)                                                                              books#show
+                #                         PATCH  /books/:id(.:format)                                                                              books#update
+                #                         PUT    /books/:id(.:format)                                                                              books#update
+                #                         DELETE /books/:id(.:format)                                                                              books#destroy
