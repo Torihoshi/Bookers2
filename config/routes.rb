33ed1cddf0f71783c3100110d4ec89e27c746957
Torihoshi <132331394+Torihoshi@users.noreply.github.com>
2023-06-19 14:09:59 +0000

    [Add]Bootstrap導入

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'homes/about' => "homes#about", as: "about"
   resources :books
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
@@ -39,3 +39,8 @@ end
                 #                         PATCH  /books/:id(.:format)                                                                              books#update
                 #                         PUT    /books/:id(.:format)                                                                              books#update
                 #                         DELETE /books/:id(.:format)                                                                              books#destroy
+                              #     users GET    /users(.:format)                                                                                  users#index
+                              # edit_user GET    /users/:id/edit(.:format)                                                                         users#edit
+                              #       user GET    /users/:id(.:format)                                                                              users#show
+                              #           PATCH  /users/:id(.:format)                                                                              users#update
+                              #           PUT    /users/:id(.:format)                                                                              users#update
